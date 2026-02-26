776dfbd4338de9e3dd858d54925877645d8ffde9
Mattya3 <m.tkm1217@ezweb.ne.jp>
2026-02-27 01:15:34 +0900

    feat: キャラクターのシンプルな左右移動の追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour{
+    [SerializeField] private float _moveSpeed;
+
+    private Vector2 _inputDirection;
+    private Rigidbody2D _rigidBody;  // _rb と略すこともあるが，初回なのでわかりやすく
+
+    void Start(){
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        _Move();
+    }
+
+    private void _Move(){
+        _rigidBody.linearVelocity = new Vector2(_inputDirection.x * _moveSpeed, _rigidBody.linearVelocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context){
+        _inputDirection = context.ReadValue<Vector2>();
     }
 }
+
