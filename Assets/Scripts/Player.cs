472f018d190b20b9dadcaf905ad48d02d8e6b421
Mattya3 <m.tkm1217@ezweb.ne.jp>
2026-02-27 18:21:49 +0900

    feat: 着地判定を追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,14 +22,20 @@ public class Player : MonoBehaviour
         _Move();
     }
 
+    /// <summary>
+    /// 移動入力を受け取ります。
+    /// </summary>
     public void OnMove(InputAction.CallbackContext context)
     {
         _inputDirection = context.ReadValue<Vector2>();
     }
 
+    /// <summary>
+    /// ジャンプ入力を受け取ります。
+    /// </summary>
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && _GroundCheck())
         {
             _rigidBody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
@@ -41,5 +47,18 @@ public class Player : MonoBehaviour
             _inputDirection.x * _moveSpeed,
             _rigidBody.linearVelocity.y
         );
+    }
+
+    private bool _GroundCheck()
+    {
+        // キャラクターの足元に接触判定を飛ばして地面に接触しているか判定
+
+        // 開始地点をプレイヤーの中心から 0.65f（足元より少し下）にずらし，自身を判定範囲から除外
+        Vector2 origin = (Vector2)transform.position + Vector2.down * 0.65f;
+        // ごく短い距離（0.005f）だけ下に判定を飛ばす
+        Vector2 boxSize = new Vector2(1f, 0.005f);
+        // ~0 はすべてのレイヤーを意味する(必要に応じて修正が必要). 何かしらが下に存在する場合trueとなる
+        RaycastHit2D hit = Physics2D.BoxCast(origin, boxSize, 0f, Vector2.down, 0.005f, ~0);
+        return hit.collider != null;
     }
 }
