15d413d0d1441a8e1a776f4d170418d4622418b7
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-01 12:37:57 +0900

    feat: targetの設定確認を追加

--- Assets/Scripts/Camera/CameraFollowController.cs
+++ Assets/Scripts/Camera/CameraFollowController.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 [RequireComponent(typeof(Camera))]
 public class CameraFollowController : MonoBehaviour
 {
     [SerializeField] private GameObject _target; // カメラが追従する対象
     [SerializeField] private Vector2 _offset; // カメラと対象の位置のオフセット
+
+    // Awake is called when the script instance is being loaded
+    void Awake()
+    {
+        if (_target == null)
+        {
+            Debug.LogError("カメラにtarget が未設定です", this);
+            enabled = false;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,9 +25,6 @@ public class CameraFollowController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_target == null)
-            return;
-
         var targetPosition = _target.transform.position;
         transform.position = new Vector3(
             targetPosition.x + _offset.x,
