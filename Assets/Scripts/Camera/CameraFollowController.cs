a4c522229da7f1e32b51e20e457ae49b17bf0683
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-01 12:27:37 +0900

    feat: オフセット機能を追加

--- Assets/Scripts/Camera/CameraFollowController.cs
+++ Assets/Scripts/Camera/CameraFollowController.cs
@@ -3,17 +3,25 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraFollowController : MonoBehaviour
 {
-    [SerializeField] private Object _target; // カメラが追従する対象
+    [SerializeField] private GameObject _target; // カメラが追従する対象
+    [SerializeField] private Vector2 _offset; // カメラと対象の位置のオフセット
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_target == null)
+            return;
+
+        var targetPosition = _target.transform.position;
+        transform.position = new Vector3(
+            targetPosition.x + _offset.x,
+            targetPosition.y + _offset.y,
+            transform.position.z
+        );
     }
 }
