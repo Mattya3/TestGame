13205908924da7be9b2b0b296c2bf84d750a8023
Mattya3 <m.tkm1217@ezweb.ne.jp>
2026-03-02 16:41:45 +0900

    feat: リスタート判定の追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
@@ -11,12 +12,19 @@ public class Player : MonoBehaviour
     [SerializeField, Range(0f, 40f)]
     private float _jumpForce;
 
+    [SerializeField]
+    private float _deathYThreshold;
+
     private Vector2 _inputDirection;
     private Rigidbody2D _rigidBody; // _rb と略すこともあるが，初回なのでわかりやすく
     private Collider2D _collider;
 
-    // 接地判定用の定数
-    private const float GROUND_CHECK_THICKNESS = 0.005f;
+    private bool _isDead = false;
+
+    private const float GROUND_CHECK_THICKNESS = 0.005f; // 接地判定用の定数
+    private const string SOLID_LAYER_NAME = "Solid"; // 上に乗れるオブジェクトのレイヤ０
+    private const string DEAD_ZONE_TAG = "DeadZone"; // リスタート発生判定用のタグ
+
 
     void Awake()
     {
@@ -26,6 +34,10 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if(_NeedRestart()){
+            _RestartStage();
+            return;
+        }
         _Move();
     }
 
@@ -58,6 +70,14 @@ public class Player : MonoBehaviour
         );
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(DEAD_ZONE_TAG))
+        {
+            _isDead = true;
+        }
+    }
+
     private bool _IsGrounded()
     {
         // コライダの境界情報（Bounds）を使用して，足元の位置と幅を動的に計算
@@ -76,9 +96,15 @@ public class Player : MonoBehaviour
             0f,
             Vector2.down,
             GROUND_CHECK_THICKNESS,
-            LayerMask.GetMask("Solid")
+            LayerMask.GetMask(SOLID_LAYER_NAME)
         );
 
         return hit.collider != null;
     }
+
+    private bool _IsFallen() => transform.position.y < _deathYThreshold;
+
+    private bool _NeedRestart() => _IsFallen() || _isDead;
+
+    private void _RestartStage() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 }
