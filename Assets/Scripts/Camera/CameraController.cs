d20f47e045d1a6af25c84bfa7bd10968650c80db
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-03 14:01:39 +0900

    fix: offsetをVector3に

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -7,7 +7,9 @@ public class CameraController : MonoBehaviour
     private GameObject _target; // 追従する対象のゲームオブジェクト
 
     [SerializeField]
-    private Vector2 _offset; // 対象からのオフセット
+    private Vector3 _offset; // 対象からのオフセット
+
+    private float _originalZ = 0f; // カメラの元のZ位置。デフォルトで0でないので、Awakeで保存しておく
 
     // Awake is called when the script instance is being loaded
     void Awake()
@@ -17,6 +19,8 @@ public class CameraController : MonoBehaviour
             Debug.LogError("カメラのtargetが設定されていません", this);
             enabled = false;
         }
+
+        _originalZ = transform.position.z; // カメラの元のZ位置を保存
     }
 
     // LateUpdate is called after all Update functions have been called
@@ -26,7 +30,7 @@ public class CameraController : MonoBehaviour
         transform.position = new Vector3(
             targetPosition.x + _offset.x,
             targetPosition.y + _offset.y,
-            transform.position.z
+            _originalZ + _offset.z
         );
     }
 }
