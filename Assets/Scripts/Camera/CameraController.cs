b73d31b810aba1de8012ad4a8a0abe1d3a944f58
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-08 00:32:43 +0900

    fix: カメラの初期位置設定を追加

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -30,6 +30,13 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+         // カメラの初期位置を設定
+        transform.position = _target.transform.position + _offset;
+    }
+
     // LateUpdate is called after all Update functions have been called
     void LateUpdate()
     {
