a8f979b29b4008dd903a63dfb1ead36aa2fd4b13
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-20 20:21:50 +0900

    feat: BGMControllerスクリプトを作成

---
+++ Assets/Scripts/Audio/BGMController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BGMController : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    [SerializeField, Min(0.0f)] private float _fadeOutTimeOnPlayerDied = 0.1f;
+    [SerializeField, Min(0.0f)] private float _fadeOutTimeOnAllPlayersGoal = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameManager.Instance.OnPlayerDied += _OnPlayerDied;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void _OnPlayerDied()
+    {
+        StartCoroutine(_CoOnPlayerDied());
+    }
+
+    private IEnumerator _CoOnPlayerDied()
+    {
+        yield return new WaitForSeconds(_fadeOutTimeOnPlayerDied);
+        _audioSource.Stop();
+    }
+}
