844749ae46280053691cf1b89ec60e7602f4b944
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-20 20:29:41 +0900

    feat: BGMのフェードアウト処理を追加

--- Assets/Scripts/Audio/BGMController.cs
+++ Assets/Scripts/Audio/BGMController.cs
@@ -9,26 +9,44 @@ public class BGMController : MonoBehaviour
     [SerializeField, Min(0.0f)] private float _fadeOutTimeOnPlayerDied = 0.1f;
     [SerializeField, Min(0.0f)] private float _fadeOutTimeOnAllPlayersGoal = 1.0f;
 
+    private void Awake()
+    {
+        if (_audioSource == null)
+        {
+            Debug.LogError("AudioSource is not assigned.");
+            enabled = false;
+            return;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameManager.Instance.OnPlayerDied += _OnPlayerDied;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameManager.Instance.OnAllPlayersGoal += _OnAllPlayersGoal;
     }
 
     private void _OnPlayerDied()
     {
-        StartCoroutine(_CoOnPlayerDied());
+        StartCoroutine(_CoFadeOut(_fadeOutTimeOnPlayerDied));
     }
 
-    private IEnumerator _CoOnPlayerDied()
+    private void _OnAllPlayersGoal()
     {
-        yield return new WaitForSeconds(_fadeOutTimeOnPlayerDied);
+        StartCoroutine(_CoFadeOut(_fadeOutTimeOnAllPlayersGoal));
+    }
+
+    private IEnumerator _CoFadeOut(float fadeOutTime)
+    {
+        var startVolume = _audioSource.volume;
+        var elapsedTime = 0.0f;
+        while (elapsedTime < fadeOutTime)
+        {
+            elapsedTime += Time.deltaTime;
+            _audioSource.volume = Mathf.Lerp(startVolume, 0.0f, elapsedTime / fadeOutTime);
+            yield return null;
+        }
         _audioSource.Stop();
+        _audioSource.volume = startVolume; // 次回再生のために音量を元に戻す
     }
 }
