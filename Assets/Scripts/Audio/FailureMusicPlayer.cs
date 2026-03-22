357789a98df63286ab518cdda4ec414fd275ce90
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-22 21:52:22 +0900

    feat: FailureMusicPlayerスクリプトを作成

---
+++ Assets/Scripts/Audio/FailureMusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class FailureMusicPlayer : MonoBehaviour
+{
+    [SerializeField] private float _delayBeforePlaying = 1f;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameManager.Instance.OnPlayerDied += _OnFailure;
+    }
+
+    private void _OnFailure()
+    {
+        StartCoroutine(_CoPlay());
+    }
+
+    private IEnumerator _CoPlay()
+    {
+        yield return new WaitForSeconds(_delayBeforePlaying);
+        _audioSource.Play();
+    }
+}
