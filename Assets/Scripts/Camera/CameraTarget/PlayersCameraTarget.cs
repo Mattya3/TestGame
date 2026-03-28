1608de768837e187ca2fa367f2a47ce92a49fd54
Wtnbyo <wtnbyopcom@gmail.com>
2026-03-29 03:15:09 +0900

    fix: Players->PlayersCameraTargetに変更

---
+++ Assets/Scripts/Camera/CameraTarget/PlayersCameraTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersCameraTarget : ICameraTarget
+{
+    private IReadOnlyList<Player> _players;
+
+    public PlayersCameraTarget(IReadOnlyList<Player> players)
+    {
+        _players = players;
+    }
+
+    public Vector3 Position()
+    {
+        if (_players.Count == 0)
+            return Vector3.zero;
+
+        var sum = Vector3.zero;
+        for (int i = 0; i < _players.Count; i++)
+        {
+            sum += _players[i].transform.position;
+        }
+        return sum / _players.Count;
+    }
+}
