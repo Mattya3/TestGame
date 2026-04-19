beb9853e63b48834e0858295699b64e6eb06ee83
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-20 00:30:48 +0900

    feat: SciFiオブジェクトに発光強度調整プロパティを追加

--- Assets/Scripts/Stages/CommonObjects/SciFiBlockMaterialConfiguration.cs
+++ Assets/Scripts/Stages/CommonObjects/SciFiBlockMaterialConfiguration.cs
@@ -17,12 +17,16 @@ public class SciFiBlockMaterialConfiguration : MonoBehaviour
     private Color _illuminationColor3 = Color.white;
 
     [SerializeField]
+    private float _illuminationIntensity = 1f;
+
+    [SerializeField]
     private Vector3 _illuminationOffsetVector = Vector3.right;
 
     private const string MASK_COLOR_PROPERTY_NAME = "_MaskColor";
     private const string ILLUMINATION_COLOR1_PROPERTY_NAME = "_IlluminationColor1";
     private const string ILLUMINATION_COLOR2_PROPERTY_NAME = "_IlluminationColor2";
     private const string ILLUMINATION_COLOR3_PROPERTY_NAME = "_IlluminationColor3";
+    private const string ILLUMINATION_INTENSITY_PROPERTY_NAME = "_IlluminationIntensity";
     private const string ILLUMINATION_OFFSET_VECTOR_PROPERTY_NAME = "_IlluminationOffsetVector";
 
     private void OnValidate()
@@ -45,6 +49,7 @@ public class SciFiBlockMaterialConfiguration : MonoBehaviour
         materialPropertyBlock.SetColor(ILLUMINATION_COLOR1_PROPERTY_NAME, _illuminationColor1);
         materialPropertyBlock.SetColor(ILLUMINATION_COLOR2_PROPERTY_NAME, _illuminationColor2);
         materialPropertyBlock.SetColor(ILLUMINATION_COLOR3_PROPERTY_NAME, _illuminationColor3);
+        materialPropertyBlock.SetFloat(ILLUMINATION_INTENSITY_PROPERTY_NAME, _illuminationIntensity);
         materialPropertyBlock.SetVector(ILLUMINATION_OFFSET_VECTOR_PROPERTY_NAME, _illuminationOffsetVector);
         renderer.SetPropertyBlock(materialPropertyBlock);
     }
