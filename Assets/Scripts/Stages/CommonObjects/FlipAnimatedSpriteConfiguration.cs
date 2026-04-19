7110387ea2c2732f3eeb565504a4d48ea4479556
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-20 03:11:40 +0900

    feat: スプライトアニメーション設定をするスクリプトを作成

---
+++ Assets/Scripts/Stages/CommonObjects/FlipAnimatedSpriteConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+[ExecuteInEditMode]
+public class FlipAnimatedSpriteConfiguration : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2Int _numSlices = Vector2Int.one;
+
+    private const string NUM_SLICES_PROPERTY_NAME = "_NumSlices";
+
+    private void OnValidate()
+    {
+        Apply();
+    }
+
+    private void Awake()
+    {
+        Apply();
+    }
+
+    private void Apply()
+    {
+        var renderer = GetComponent<Renderer>();
+        var materialPropertyBlock = new MaterialPropertyBlock();
+
+        var floatNumSlices = new Vector2(_numSlices.x, _numSlices.y);
+
+        renderer.GetPropertyBlock(materialPropertyBlock);
+        materialPropertyBlock.SetVector(NUM_SLICES_PROPERTY_NAME, floatNumSlices);
+        renderer.SetPropertyBlock(materialPropertyBlock);
+    }
+}
