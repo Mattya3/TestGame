ce7e1c4ac626dfef5c877a2d2aff32d370125206
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-21 06:27:59 +0900

    feat: マスク制御のスクリプトを追加

---
+++ Assets/Scripts/Effects/WipingMaskController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class WipingMaskController : MonoBehaviour
+{
+    [SerializeField]
+    private float _maskThreshold = 0.0f;
+
+    private Material _material;
+
+    private static readonly int MaskThresholdID = Shader.PropertyToID("_MaskThreshold");
+
+    private void Awake()
+    {
+        _material = GetComponent<Image>().material;
+        if (_material == null)
+        {
+            Debug.LogError("WipingMaskController requires an Image component with a material.");
+            enabled = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_material != null)
+        {
+            Destroy(_material);
+            _material = null;
+        }
+    }
+
+    private void Update()
+    {
+        _material.SetFloat(MaskThresholdID, _maskThreshold);
+    }
+}
