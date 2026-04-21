064fc01b2d139f277ce10dbc9271a324defa2ccd
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-22 02:29:45 +0900

    refactor: 不要なusingを削除

--- Assets/Scripts/Game/GameEventTrigger.cs
+++ Assets/Scripts/Game/GameEventTrigger.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-using UnityEngine.Events;
 using static Constants;
 
 public static class GameEventTrigger
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using static Constants;
 
 [RequireComponent(typeof(ScreenEffectsAccess))]
