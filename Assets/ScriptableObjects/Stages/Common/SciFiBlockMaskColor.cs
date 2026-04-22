a0befe4b7c6e3083d4725514b529d6aef49f03fe
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-23 02:09:06 +0900

    feat: SciFiBlockのマテリアル設定をScriptableObjectとして作成

---
+++ Assets/ScriptableObjects/Stages/Common/SciFiBlockMaskColor.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSciFiBlockMaskColor", menuName = "Scriptable Objects/Stages/Common/SciFiBlockMaskColor")]
+public class SciFiBlockMaskColor : ScriptableObject
+{
+    public Color color = Color.white;
+}
