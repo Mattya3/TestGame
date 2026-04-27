e213e886f9e90f9e1e96538916cbe804e1933219
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-27 17:28:21 +0900

    feat: 開始カメラターゲットクラスを作成

---
+++ Assets/Scripts/Camera/CameraTarget/OpeningCameraTarget.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class OpeningCameraTarget : MonoBehaviour, ICameraTarget
+{
+    [SerializeField]
+    private Vector3 _positionBegin;
+
+    [SerializeField]
+    private Vector3 _positionEnd;
+
+    [SerializeField]
+    private float _progressFactor = 0f; // 0から1の範囲で、Animatorを使って進行度を制御するための値
+
+    public void OnStart()
+    {
+    }
+
+    public Vector3 Position
+    {
+        get
+        {
+            // Animatorの進行度に基づいて、開始位置と終了位置の間を線形補間する
+            float progress = Mathf.Clamp01(_progressFactor);
+            return Vector3.Lerp(_positionBegin, _positionEnd, progress);
+        }
+    }
+
+    public bool EnableCollider => false; // カメラのコライダーは無効
+}
