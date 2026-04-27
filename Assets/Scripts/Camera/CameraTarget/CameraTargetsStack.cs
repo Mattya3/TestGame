2a78f4b31b04e6014b6aecd4c069323d06d78cfa
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-27 16:15:29 +0900

    feat: CameraTargetsStackクラスを作成

---
+++ Assets/Scripts/Camera/CameraTarget/CameraTargetsStack.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// カメラターゲットのスタック
+// 最上位のターゲットが現在のカメラターゲットとなる
+public class CameraTargetsStack
+{
+    private Stack<ICameraTarget> _stack = new Stack<ICameraTarget>();
+
+    public CameraTargetsStack(ICameraTarget[] targetsArray)
+    {
+        if (targetsArray == null)
+        {
+            Debug.LogError("CameraTargetsStackのコンストラクタにはICameraTargetの配列が必要です");
+            return;
+        }
+        if (targetsArray.Length == 0)
+        {
+            Debug.LogError("CameraTargetsStackのコンストラクタには少なくとも1つのICameraTargetが必要です");
+            return;
+        }
+
+        // 配列の順序を逆にしてスタックに積む
+        for (int i = targetsArray.Length - 1; i >= 0; i--)
+            _stack.Push(targetsArray[i]);
+    }
+
+    public void Push(ICameraTarget target)
+    {
+        _stack.Push(target);
+        target.OnStart();
+    }
+
+    public void Pop()
+    {
+        if (_stack.Count > 0)
+            _stack.Pop();
+    }
+
+    public void Start()
+    {
+        if (_stack.Count > 0)
+            _stack.Peek().OnStart();
+    }
+
+    public Vector3 Position {
+        get
+        {
+            if (_stack.Count > 0)
+                return _stack.Peek().Position;
+            else
+                return Vector3.zero; // スタックが空の場合は原点を返す
+        }
+    }
+}
