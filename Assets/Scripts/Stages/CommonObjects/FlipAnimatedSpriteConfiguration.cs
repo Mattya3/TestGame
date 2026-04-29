03d8281cbcba198a1ef3262c8ad3b61222fad29c
Wtnbyo <wtnbyopcom@gmail.com>
2026-04-30 01:56:25 +0900

    feat: ランダムパターンを手動設定可能に

--- Assets/Scripts/Stages/CommonObjects/FlipAnimatedSpriteConfiguration.cs
+++ Assets/Scripts/Stages/CommonObjects/FlipAnimatedSpriteConfiguration.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
-[ExecuteInEditMode]
 public class FlipAnimatedSpriteConfiguration : MonoMaterialAccessBehaviour
 {
     [SerializeField]
     private Vector2Int _numSlices = Vector2Int.one;
 
+    [SerializeField, Range(0, 16)]
+    private int _patternIndexOffset = 0;
+
     private const string NUM_SLICES_PROPERTY_NAME = "_NumSlices";
+    private const string PATTERN_INDEX_OFFSET_PROPERTY_NAME = "_PatternIndexOffset";
+
+    protected override void OnValidate()
+    {
+        if (_patternIndexOffset < 0 || _patternIndexOffset >= _numSlices.x)
+            _patternIndexOffset = Mathf.Clamp(_patternIndexOffset, 0, _numSlices.x - 1);
+
+        base.OnValidate();
+    }
 
     protected override void SetMaterialProperties(MaterialPropertyBlock materialPropertyBlock)
     {
         var floatNumSlices = new Vector2(_numSlices.x, _numSlices.y);
         materialPropertyBlock.SetVector(NUM_SLICES_PROPERTY_NAME, floatNumSlices);
+        materialPropertyBlock.SetFloat(PATTERN_INDEX_OFFSET_PROPERTY_NAME, _patternIndexOffset);
     }
 }
