1067c4e76438edfb57b9d83d8d8cb31390bc4ea0
Wtnbyo <wtnbyopcom@gmail.com>
2026-06-01 22:41:05 +0900

    refactor: SceneTransitionManager -> SceneTransitionTriggerに

--- Assets/Scripts/Game/SceneTransitionTrigger.cs
+++ Assets/Scripts/Game/SceneTransitionTrigger.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransitionManager : MonoEventReactingBehaviour
+public class SceneTransitionTrigger : MonoEventReactingBehaviour
 {
     private enum TransitionMode
     {
