471c4e82a42e833a2f01aecfc4b0a3e9a36f7c86
Wtnbyo <wtnbyopcom@gmail.com>
2026-06-27 16:46:58 +0900

    fix: 不要なメソッドを削除

--- Assets/Scripts/Characters/Player/PlayersCollection/PlayersCollectionMutableAccess.cs
+++ Assets/Scripts/Characters/Player/PlayersCollection/PlayersCollectionMutableAccess.cs
@@ -2,11 +2,6 @@
 
 public class PlayersCollectionMutableAccess : AccessComponent<IPlayersCollection>
 {
-    public void RegisterPlayer(Player player)
-    {
-        Reference?.RegisterPlayer(player);
-    }
-
     public void SetMoveController(IMoveController moveController)
     {
         Reference?.SetMoveController(moveController);
