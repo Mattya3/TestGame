0d576cb8095d8f49f987c8b9b9f392f1bf47274d
Wtnbyo <wtnbyopcom@gmail.com>
2026-07-03 20:46:34 +0900

    fix: IsDirtyを実装

--- Assets/Scripts/Effects/WipingMaskController.cs
+++ Assets/Scripts/Effects/WipingMaskController.cs
@@ -5,10 +5,15 @@ public class WipingMaskController : MonoUIImageMaterialAccessBehaviour
     [SerializeField]
     private float _maskThreshold = 0.0f;
 
+    private float _lastMaskThreshold = float.MaxValue;
+
     private static readonly int MaskThresholdID = Shader.PropertyToID("_MaskThreshold");
 
     protected override void SetMaterialProperties(Material material)
     {
         material.SetFloat(MaskThresholdID, _maskThreshold);
+        _lastMaskThreshold = _maskThreshold;
     }
+
+    protected override bool IsDirty => !Mathf.Approximately(_maskThreshold, _lastMaskThreshold);
 }
