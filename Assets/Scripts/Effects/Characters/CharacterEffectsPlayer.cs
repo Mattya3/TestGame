d318cdb14eb60bacb1dff694acb40897efd778ae
Wtnbyo <wtnbyopcom@gmail.com>
2026-08-01 00:18:42 +0900

    feat: CharacterEffectsPlayerクラスを作成

---
+++ Assets/Scripts/Effects/Characters/CharacterEffectsPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class CharacterEffectsPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private VisualEffectsPool.EffectConfig _deadZoneDeathEffect;
+
+    [SerializeField]
+    private VisualEffectsPool.EffectConfig _fallDeathEffect;
+
+    private VisualEffectsPool.IEffectPlayer _deadZonePlayer;
+    private VisualEffectsPool.IEffectPlayer _fallPlayer;
+
+    private void Awake()
+    {
+        _deadZonePlayer = _deadZoneDeathEffect.CreateEffectPlayer(transform);
+        _fallPlayer = _fallDeathEffect.CreateEffectPlayer(transform);
+    }
+
+    private void OnDestroy()
+    {
+        _deadZonePlayer?.Cleanup(this);
+        _fallPlayer?.Cleanup(this);
+    }
+
+    public void PlayDeathEffect(Constants.DeathReason deathReason)
+    {
+        VisualEffectsPool.IEffectPlayer player = null;
+        switch (deathReason)
+        {
+            case Constants.DeathReason.DeadZone:
+                player = _deadZonePlayer;
+                break;
+            case Constants.DeathReason.Fall:
+                player = _fallPlayer;
+                break;
+            default:
+                Debug.LogError($"Unhandled DeathReason value: {deathReason}");
+                return;
+        }
+
+        player?.Play(this);
+    }
+}
