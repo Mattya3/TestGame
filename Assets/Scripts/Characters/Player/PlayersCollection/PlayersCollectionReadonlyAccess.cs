72eb948e6c74edb62f155bb8271c1f2b00456cae
Wtnbyo <wtnbyopcom@gmail.com>
2026-08-12 22:45:21 +0900

    fix: 参照がない場合の空リストをキャッシュ

--- Assets/Scripts/Characters/Player/PlayersCollection/PlayersCollectionReadonlyAccess.cs
+++ Assets/Scripts/Characters/Player/PlayersCollection/PlayersCollectionReadonlyAccess.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class PlayersCollectionReadonlyAccess : AccessComponent<IPlayersCollection>
 {
+    // 参照がない場合に返す空のコレクションをキャッシュしておく
+    private ReadOnlyCollection<Vector3> _emptyPositions = new ReadOnlyCollection<Vector3>(new List<Vector3>());
+    private ReadOnlyCollection<Bounds> _emptyBoundsList = new ReadOnlyCollection<Bounds>(new List<Bounds>());
+    private ReadOnlyCollection<Vector2> _emptyInputDirections = new ReadOnlyCollection<Vector2>(new List<Vector2>());
+
     public int Count => Reference != null ? Reference.Count : 0;
 
     public ReadOnlyCollection<Vector3> Positions =>
         Reference != null
             ? Reference.Positions
-            : new ReadOnlyCollection<Vector3>(new List<Vector3>());
+            : _emptyPositions;
 
     public ReadOnlyCollection<Bounds> BoundsList =>
         Reference != null
             ? Reference.BoundsList
-            : new ReadOnlyCollection<Bounds>(new List<Bounds>());
+            : _emptyBoundsList;
 
     public ReadOnlyCollection<Vector2> InputDirections =>
         Reference != null
             ? Reference.InputDirections
-            : new ReadOnlyCollection<Vector2>(new List<Vector2>());
+            : _emptyInputDirections;
 }
