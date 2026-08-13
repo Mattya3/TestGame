023adfc55030a2b0acb9a24a00bbce3d758e1bdd
Wtnbyo <wtnbyopcom@gmail.com>
2026-08-13 19:16:20 +0900

    feat: HoleMaskControllerクラスを作成

---
+++ Assets/Scripts/Effects/ScreenEffects/HoleMaskController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HoleMaskController : MonoUIImageMaterialAccessBehaviour
+{
+    [SerializeField]
+    private float _maskThreshold = 0.0f;
+
+    private float _lastMaskThreshold = float.MaxValue;
+
+    private static readonly int MaskThresholdID = Shader.PropertyToID("_MaskThreshold");
+
+    protected override bool IsMaterialValid(Material material)
+    {
+        if (!material.HasProperty(MaskThresholdID))
+        {
+            Debug.LogError(
+                $"Material {material.name} does not have a property named '_MaskThreshold'."
+            );
+            return false;
+        }
+        return true;
+    }
+
+    protected override void SetMaterialProperties(Material material)
+    {
+        material.SetFloat(MaskThresholdID, _maskThreshold);
+        _lastMaskThreshold = _maskThreshold;
+    }
+
+    protected override bool IsDirty => !Mathf.Approximately(_maskThreshold, _lastMaskThreshold);
+}
