a72bcb063faaa4344a8a6b076a59139614b62db6
Wtnbyo <wtnbyopcom@gmail.com>
2026-08-14 21:41:31 +0900

    fix: CameraMutableAccessを非singletonに

--- Assets/Scripts/Camera/CameraMutableAccess.cs
+++ Assets/Scripts/Camera/CameraMutableAccess.cs
@@ -2,24 +2,6 @@
 
 public class CameraMutableAccess : AccessComponent<CameraController>
 {
-    static CameraMutableAccess _instance;
-
-    private void Awake()
-    {
-        if (_instance != null)
-        {
-            Debug.LogError("Multiple CameraAccess instances detected. This is not allowed.");
-            return;
-        }
-        _instance = this;
-    }
-
-    private void OnDestroy()
-    {
-        if (_instance == this)
-            _instance = null;
-    }
-
     public void PushTarget(ICameraTarget target)
     {
         Reference?.PushTarget(target);
