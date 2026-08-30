31ce3f8ac23740ea32bda1833c5121cdc906ad72
Wtnbyo <wtnbyopcom@gmail.com>
2026-08-31 01:39:34 +0900

    feat: 固定カメラターゲットを追加

---
+++ Assets/Scripts/Camera/CameraTarget/FixedCameraTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FixedCameraTarget : MonoBehaviour, ICameraTarget
+{
+    [SerializeField]
+    private Vector3 _position;
+
+    public bool IsActive => enabled;
+
+    public void OnStart()
+    {
+        enabled = true;
+    }
+
+    public Vector3 Position => _position;
+
+    public bool EnableCollider => false;
+}
